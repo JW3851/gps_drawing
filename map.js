296d1f1580fa742a9679e70713f8bab415e8eed0
JW3851 <62093012+JW3851@users.noreply.github.com>
2020-04-02 07:22:43 -0400

    Update map.js

--- map.js
+++ map.js
@@ -2,11 +2,11 @@
 
 console.log('Loaded map.js')
 
-mapboxgl.accessToken = 'pk.eyJ1IjoianczODUxIiwiYSI6ImNrN29qcWU2MTBhNWkzZXFxNnQ0OThka2sifQ.tDkhsqVfmARI4pSB5G3CZw'
+mapboxgl.accessToken = 'YOUR TOKEN HERE'
 
 let map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/jw3851/ck8imoj300w6l1intk5afj1i8',
+    style: 'YOUR STYLE URL HERE',
     center: [-73.96216,40.80779],
     zoom: 16
 })
@@ -35,20 +35,4 @@ map.addControl(geolocate, 'top-left')
 
 geolocate.on('geolocate', function(event) {
 
-})
-
-// create a variable to keep track of the user's current location
-// we're going to initialize it to the default center of the map
-let current_location = [-73.96216, 40.80779]
-
-// update the variable whenever a geolocation event fires
-geolocate.on('geolocate', function(event) {
-    current_location = [event.coords.longitude, event.coords.latitude]
-    console.log('geolocated', current_location)   
-})
-
-// for testing purposes, also update the variable whenever you click on the map
-map.on('click', function(event) {
-    current_location = [event.lngLat.lng, event.lngLat.lat]
-    console.log('clicked', current_location)        
 })
