126bf09468d50e26b1b8566638a716e7a7de010f
JW3851 <62093012+JW3851@users.noreply.github.com>
2020-04-02 06:55:37 -0400

    Update map.js

--- map.js
+++ map.js
@@ -1,1 +1,15 @@
+// create a variable to keep track of the user's current location
+// we're going to initialize it to the default center of the map
+let current_location = [-73.96216, 40.80779]
 
+// update the variable whenever a geolocation event fires
+geolocate.on('geolocate', function(event) {
+    current_location = [event.coords.longitude, event.coords.latitude]
+    console.log('geolocated', current_location)   
+})
+
+// for testing purposes, also update the variable whenever you click on the map
+map.on('click', function(event) {
+    current_location = [event.lngLat.lng, event.lngLat.lat]
+    console.log('clicked', current_location)        
+})
